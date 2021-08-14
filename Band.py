e409434764d2314c507cbfd33268d02c92e3c2bf
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-14 13:08:56 +0530

    Update Band.py

--- Band.py
+++ Band.py
@@ -7,5 +7,6 @@ a = input("What's name of the city you grew up in? \n")
 # Your pet name will be the second word in your band name
 b = input("What's your pet's name? \n")
 
+# Your final output
 print("Your Band Name is " + a + " " + b)
 
