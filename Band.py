817cd9b1a609d1260fe21bb8da4de142c2203f9e
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-14 13:05:01 +0530

    Update Band.py

--- Band.py
+++ Band.py
@@ -1,6 +1,7 @@
 # it will be printed on the top
 print("Welcome to Band Name Generator")
 
+# Yoy name will be the first word in your band name
 a = input("What's name of the city you grew up in? \n")
 
 b = input("What's your pet's name? \n")
